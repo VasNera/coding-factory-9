c60bbaa844741017e652b5ff3b1c005ef8564c77
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 14:52:37 +0200

    feat: add interface ITeacherService

---
+++ src/gr/aueb/cf/cf9/ch15/schoolapp/ITeacherService.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.cf9.ch15.schoolapp;
+
+/**
+ * provides most common CRUD operations.
+ */
+
+public interface ITeacherService {
+
+    void addTeacher(Teacher teacher);
+
+    void updateTeacher(Teacher teacher);
+
+    void removeTeacher(long id);
+
+    Teacher getTeacherById( long id);
+
+    Teacher[] getAllTeachers();
+}
