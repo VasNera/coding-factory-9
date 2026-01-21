9906bc363c1d294132cb13a9767e7e1349a9cafc
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 16:11:34 +0200

    feat: add InsufficientBalanceException

---
+++ src/gr/aueb/cf/cf9/ch18/bankApp/core/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.ch18.bankApp.core.exceptions;
+
+public class InsufficientBalanceException extends Exception{
+
+    public InsufficientBalanceException(String message){
+        super(message);
+
+    }
+}
