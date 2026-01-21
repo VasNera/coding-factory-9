da575fbcf18ce836dcc74381c97c43106dc98d28
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 16:11:57 +0200

    feat: add NegativeAmountException

---
+++ src/gr/aueb/cf/cf9/ch18/bankApp/core/exceptions/NegativeAmountException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.ch18.bankApp.core.exceptions;
+
+public class NegativeAmountException extends Exception{
+
+    public NegativeAmountException(String message){
+        super(message);
+
+    }
+}
