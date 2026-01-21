f0b6ec7111e42a83395a901d01de9e2249e2b642
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 16:11:13 +0200

    feat: add AccountNotFoundException

---
+++ src/gr/aueb/cf/cf9/ch18/bankApp/core/exceptions/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.ch18.bankApp.core.exceptions;
+
+public class AccountNotFoundException extends Exception{
+
+    public AccountNotFoundException(String message){
+        super(message);
+
+    }
+}
