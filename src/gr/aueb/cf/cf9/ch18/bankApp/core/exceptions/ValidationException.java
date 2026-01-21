ea4f6d737ed84a0ad9e8d31cd8ab26c7123053da
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 16:12:28 +0200

    feat: add ValidationException

---
+++ src/gr/aueb/cf/cf9/ch18/bankApp/core/exceptions/ValidationException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.cf9.ch18.bankApp.core.exceptions;
+
+public class ValidationException extends Exception{
+
+    public ValidationException(String message){
+        super(message);
+    }
+}
