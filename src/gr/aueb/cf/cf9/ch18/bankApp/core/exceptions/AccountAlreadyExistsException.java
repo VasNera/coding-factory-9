a3ba5a4bbcc8f8bdfe844709f0f1f1b3e7ca36fe
Vas. Neratzis <vas.neratzis@gmail.com>
2026-01-21 16:10:52 +0200

    feat: add AccountAlreadyExistsException

---
+++ src/gr/aueb/cf/cf9/ch18/bankApp/core/exceptions/AccountAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.ch18.bankApp.core.exceptions;
+
+public class AccountAlreadyExistsException extends Exception{
+
+    public AccountAlreadyExistsException(String message){
+        super(message);
+
+    }
+}
